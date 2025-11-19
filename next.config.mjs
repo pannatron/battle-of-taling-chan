0da52f78c74f6c105d9f0ddf677088e12c0e0468
pannatorn <57486797+pannatron@users.noreply.github.com>
2025-11-20 03:24:57 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,13 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
